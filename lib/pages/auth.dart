0c103a093e795ca269f1622553fef29ad4144678
Madhav <madhavbharadwaj1995@gmail.com>
2019-06-19 10:49:51 +0530

    till lecture 141

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -8,9 +8,17 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String _emailValue;
-  String _passwordValue;
-  bool _acceptTerms = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Map<String, dynamic> _formData = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false,
+  };
+
+  // String _emailValue;
+  // String _passwordValue;
+
+  //bool _acceptTerms = false;
   DecorationImage _buildBackgroundImage() {
     return DecorationImage(
       fit: BoxFit.cover,
@@ -21,77 +29,97 @@ class _AuthPageState extends State<AuthPage> {
   }
 
   Widget _buildEMailTextField() {
-    return TextField(
+    return TextFormField(
+        validator: (String value) {
+          if (value.isEmpty ||
+              !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                  .hasMatch(value)) {
+            return 'Please enter a valid email';
+          }
+        },
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
             labelText: "E-Mail", filled: true, fillColor: Colors.white),
-        onChanged: (String value) {
-          setState(() {
-            _emailValue = value;
-          });
+        onSaved: (String value) {
+          _formData['email'] = value;
         });
   }
 
   Widget _buildPasswordTextField() {
-    return TextField(
+    return TextFormField(
+        validator: (String value) {
+          if (value.isEmpty || value.length < 8) {
+            return 'Password is required and should be 8+ characters';
+          }
+        },
         obscureText: true,
         decoration: InputDecoration(
             labelText: "Password", filled: true, fillColor: Colors.white),
-        onChanged: (String value) {
-          setState(() {
-            _passwordValue = value;
-          });
+        onSaved: (String value) {
+          _formData['password'] = value;
         });
   }
 
   Widget _buildAcceptSwitch() {
     return SwitchListTile(
       title: Text("Accept Terms"),
-      value: _acceptTerms,
+      value: _formData['acceptTerms'],
       onChanged: (bool value) {
         setState(() {
-          _acceptTerms = value;
+          _formData['acceptTerms'] = value;
         });
       },
     );
   }
 
   void _submitForm() {
-    print(_emailValue);
-    print(_passwordValue);
+    if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
+      return;
+    }
+    _formKey.currentState.save();
+    print(_formData);
 
     Navigator.pushReplacementNamed(context, '/products');
   }
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
     return Scaffold(
         appBar: AppBar(
           title: Text('Login'),
         ),
         body: Container(
-            decoration: BoxDecoration(
-              image: _buildBackgroundImage(),
-            ),
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    _buildEMailTextField(),
-                    _buildPasswordTextField(),
-                    SizedBox(height: 10.0),
-                    _buildAcceptSwitch(),
-                    SizedBox(height: 10.0),
-                    RaisedButton(
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      child: Text("LOGIN"),
-                      onPressed: _submitForm,
-                    ),
-                  ],
+          decoration: BoxDecoration(
+            image: _buildBackgroundImage(),
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: targetWidth,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      _buildEMailTextField(),
+                      SizedBox(height: 10.0),
+                      _buildPasswordTextField(),
+                      SizedBox(height: 10.0),
+                      _buildAcceptSwitch(),
+                      SizedBox(height: 10.0),
+                      RaisedButton(
+                        textColor: Colors.white,
+                        child: Text("LOGIN"),
+                        onPressed: _submitForm,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            )));
+            ),
+          ),
+        ));
   }
 }
